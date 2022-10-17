24fc189054cc87bf2dfa06ce8aec84c7f415ed37
Jose <al_jperaltaa@iessierradelasvillas.org>
2022-10-17 11:15:51 +0200

    AGENDA DE TELEFONO CON MENU

---
+++ EJERCICIO 10.py
@@ -0,0 +1,6 @@
+'''Algoritmo que pida los puntos centrales x1,y1,x2,y2 y los radios r1,r2 de dos circunferencias y las clasifique en uno de estos estados:
+-exteriores
+-tangentes exteriores
+-secantes
+-tangentes interiores
+-interiores concéntricas'''
--- EJERCICIO 9.py
+++ EJERCICIO 9.py
@@ -5,18 +5,21 @@ num3=0
 num1=int(input("Dime un número:"))
 num2=int(input("Dime otro número:"))
 num3=int(input("Dime el último número:"))
-if (numero1>numero2) and (numero1>numero3):
-    if (numero2>numero3):
-        print("Los números de mayor a menor serían:", numero1, numero2, numero3)
-    else:
-        print("Los números de mayor a menor serían:", numero1, numero3, numero2)
-if (numero2>numero1) and (numero2>numero3):
-    if (numero1>numero3):
-        print("Los números de mayor a menor serían:", numero2, numero1, numero3)
-    else:
-        print("Los números de mayor a menor serían:", numero2, numero3, numero1)        
-if (numero3>numero1) and (numero3>numero2):
-    if (numero1>numero2):
-        print("Los números de mayor a menor serían:", numero3, numero1, numero2)
-    else:
-        print("Los números de mayor a menor serían:", numero3, numero2, numero1)    
+if (num1>num2) and (num2>num3):
+
+    print("Los números ordenados de mayor a menor son: ", num1,",",num2,",",num3)
+if (num1>num3) and (num3>num2):
+
+    print("Los números ordenados de mayor a menor son: ", num1,",",num3,",",num2)
+
+if (num2>num1) and (num1>num3):
+
+    print("Los números ordenados de mayor a menor son: ", num2,",",num1,",",num3)
+if (num2>num3) and (num3>num1):
+
+    print("Los números ordenados de mayor a menor son: ", num2,",",num3,",",num1)
+if (num3>num1) and (num1>num2):
+
+    print("Los números ordenados de mayor a menor son: ", num2,",",num1,",",num3)
+else:
+    print("Los números ordenados de mayor a menor son: ", num3,",",num2,",",num1)
